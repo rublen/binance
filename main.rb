453741173288f9c08b9df5fbe6d969741a02e30d
lena <rublen77@gmail.com>
2018-11-21 16:20:27 +0200

    initial commit (corrected)

--- main.rb
+++ main.rb
@@ -16,17 +16,10 @@ end
 
 ActiveRecord::Base.establish_connection(db_configuration["development"])
 
-# Credential.create!(key: 'oe9QZ7YcAAQgO4X6o5C2UTO3UORu8RXvBn6wIW7qCC0ah2bME1PGNfcwVtMwh1Fa',
-#                    secret: 'hW3NxHfd6v6AeSvDVAPdJBnjIjvhpLj3Am5wLjPYl2ozTmCubKEWnciEvLxDIQjv')
-
 client = Binance.new(Credential.last)
 
-pp 'Hystorical Trades:', JSON.parse(client.hystorical_trades_call('ETHBTC', 5).body)
-
+pp arr = JSON.parse(client.hystorical_trades_call('ETHBTC', 5).body)
+a = arr.select { |h| h['id'] = 91857071 }
+pp a
 # pp 'Hystorical Trades:', JSON.parse(client.public_call.body)
 # pp('Hystorical Trades:', JSON.parse(client.public_call('/api/v1/exchangeInfo').body))
-
-# Credential.create!(key: 'oe9QZ7YcAAQgO4X6o5C2UTO3UORu8RXvBn6wIW7qCC0ah2bME1PGNfcwVtMwh1Fa',
-#                    secret: 'hW3NxHfd6v6AeSvDVAPdJBnjIjvhpLj3Am5wLjPYl2ozTmCubKEWnciEvLxDIQjv')
-
-# p Credential.all
